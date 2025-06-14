9787a6f28f2f2bd76fcb4116814d43d964c332b3
Madhusudan Dadhich <madhusudan.dadhich@joshtechnologygroup.com>
2025-06-14 08:59:42 +0530

    Created header section containing navbar, which is responsive for all screen sizes and functionality like collapsible nav menu
    added index files to manage scss files as modules
    
    updated styles close to pixel perfect

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,31 @@
 import '../styles/main.scss';
+
+// Utilities
+
+/**
+ * Function to toggle hamburger state - active or inactive
+ */
+const toggleHamburger = () => {
+    navbar.classList.toggle('active');
+    authWrapper.classList.toggle('active');
+};
+
+// Navbar Hamburger Toggle
+const navbar = document.querySelector('[data-navbar]');
+const authWrapper = document.querySelector('[data-auth]');
+const navbarToggler = document.querySelector('[data-navbar-toggler]');
+
+navbarToggler.addEventListener('click', toggleHamburger);
+
+// Sticky Header
+const header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', () => {
+    header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');
+    if (
+        navbar.classList.contains('active') ||
+        authWrapper.classList.contains('active')
+    ) {
+        toggleHamburger();
+    }
+});
